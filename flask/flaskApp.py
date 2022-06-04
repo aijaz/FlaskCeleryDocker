a7709c4349e6ade206f8c10579346edf5dea991e
Aijaz Ansari <aijaz@users.noreply.github.com>
2022-06-03 18:19:43 -0600

    Added a sleep time

--- flask/flaskApp.py
+++ flask/flaskApp.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from random import randint
+from time import sleep
+
 
 app = Flask(__name__)
 
@@ -6,12 +9,14 @@ app = Flask(__name__)
 @app.route("/flask/heavy")
 def accept_heavy_task_request():
 
-    result = do_heavy_task()
+    sleep_time = randint(5, 10)
+    result = do_heavy_task(sleep_time)
     return {"result": result}
 
 
-def do_heavy_task():
-    return "This task is done."
+def do_heavy_task(sleep_time):
+    sleep(sleep_time)
+    return f"This task took {sleep_time} seconds."
 
 
 if __name__ == "__main__":
