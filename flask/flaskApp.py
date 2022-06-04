45a965ad99d5421ca60fc4e560cc40617e97415e
Aijaz Ansari <aijaz@users.noreply.github.com>
2022-06-03 20:49:09 -0600

    Celery without bind

--- flask/flaskApp.py
+++ flask/flaskApp.py
@@ -1,19 +1,21 @@
 from flask import Flask
 from random import randint
 from time import sleep
+from celery import Celery
 
 
 app = Flask(__name__)
+celery = Celery(broker="amqp://localhost//")
 
 
 @app.route("/flask/heavy")
 def accept_heavy_task_request():
-
     sleep_time = randint(5, 10)
-    result = do_heavy_task(sleep_time)
-    return {"result": result}
+    task = do_heavy_task.delay(sleep_time)
+    return {"result": task.task_id}
 
 
+@celery.task(name="heavy_task")
 def do_heavy_task(sleep_time):
     sleep(sleep_time)
     return f"This task took {sleep_time} seconds."
